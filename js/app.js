f351599032f49dbcebfe27c481cf0d3df254fb11
admin-moriris <admin@moriris.com>
2025-11-20 00:21:59 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // /js/app.js
 export const SUPABASE_URL = "https://sqwyfscumunyhsvsvejv.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR_PUBLIC_ANON_KEY"; // SupabaseのAnonキー
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxd3lmc2N1bXVueWhzdnN2ZWp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0ODM4NDQsImV4cCI6MjA3NzA1OTg0NH0.P_6eP5E_Du82IEI1gzn4PKQjnqlVGoptmZZR9aIiXNU"; // SupabaseのAnonキー
 
 export const sb = window.supabase.createClient(
   SUPABASE_URL,
